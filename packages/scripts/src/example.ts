df888ab7687dd823dfd4294fb8aeda2fe1a8561e
Aaron Pennington <smokeyblues@hotmail.com>
2024-11-25 17:14:51 -0700

    Update example.ts
    Aligning project name with repo name

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@aws-sst-v4-notes/core/example";
+import { Example } from "@aws-sstv4-notes/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
