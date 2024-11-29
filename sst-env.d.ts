568e1314b1231939572bcb6a6d7b177dc1ffbd41
smokeyblues <aaronpennington@Aarons-Mac-mini.local>
2024-11-29 14:44:54 -0700

    Updating the sst version

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,51 +3,6 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
-    Frontend: {
-      type: "sst.aws.StaticSite"
-      url: string
-    }
-    IdentityPool: {
-      id: string
-      type: "sst.aws.CognitoIdentityPool"
-    }
-    Notes: {
-      name: string
-      type: "sst.aws.Dynamo"
-    }
-    NotesApi: {
-      type: "sst.aws.ApiGatewayV2"
-      url: string
-    }
-    StripePublicKey: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-    StripeSecretKey: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-    Uploads: {
-      name: string
-      type: "sst.aws.Bucket"
-    }
-    UserPool: {
-      id: string
-      type: "sst.aws.CognitoUserPool"
-    }
-    UserPoolClient: {
-      id: string
-      secret: string
-      type: "sst.aws.CognitoUserPoolClient"
-    }
-    Users: {
-      name: string
-      type: "sst.aws.Dynamo"
-    }
-    UsersApi: {
-      type: "sst.aws.ApiGatewayV2"
-      url: string
-    }
   }
 }
 export {}
